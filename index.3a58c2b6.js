ae3625bd8469c6ef36593d6ae9eb7aec08197954
GrzegorzKR <91796260+GrzegorzKR@users.noreply.github.com>
2022-12-03 18:54:44 +0000

    Deploying to gh-pages from @ GrzegorzKR/goit-js-hw-10@59e3e1eedd89288a4d467d92ba0b5eedb1d973f4 🚀

---
+++ index.3a58c2b6.js
@@ -0,0 +1,2 @@
+console.log("TEST"),console.log("test2");
+//# sourceMappingURL=index.3a58c2b6.js.map
---
+++ index.9d154268.js
@@ -0,0 +1,2 @@
+console.log("TEST"),console.log("test2");
+//# sourceMappingURL=index.9d154268.js.map
